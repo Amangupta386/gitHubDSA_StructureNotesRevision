1b2cd652c1a560bedd241de61befc274b7e36a57
Amangupta386 <95014109+Amangupta386@users.noreply.github.com>
2022-07-08 09:53:32 +0530

    Update Merge Sorted Array.cpp

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -1,22 +1,11 @@
-//linked of ques.
-//https://leetcode.com/problems/merge-sorted-array/
-//two Pointer approach
-int i=m-1,j=n-1,k=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                k--; i--;
-            }
-            else{
-                nums1[k]=nums2[j];
-                k--; j--;
-            }
-        }
-        while(i>=0){
-            nums1[k--]=nums1[i--];
-        }
-        while(j>=0){
-            nums1[k--]=nums2[j--];
-        }
-
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+           
+			for(int i=m; i<m+n; i++){
+				nums1[i] = nums2[i-m];
+			}
+		sort(nums1.begin(), nums1.end());
     }
+
+};
