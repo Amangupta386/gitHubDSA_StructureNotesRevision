a4c5bd56f66f1680b4c5a5cb08ad8ccf04250bef
Amangupta386 <95014109+Amangupta386@users.noreply.github.com>
2022-07-23 07:37:18 +0530

    Create valid_palindrome.cpp

---
+++ valid_palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+char tolowercase(char ch){
+    if(ch>='a' && ch<='z' )
+        return ch;
+    else{
+        ch=ch-'A'+'a';
+        return ch;
+    }
+}
+    bool isPalindrome(string s) {
+        
+    int i=0,j=s.length()-1;
+    while(i<=j){
+        if(isalnum(s[i])==0)
+            i++;
+        else if(isalnum(s[j])==0)
+            j--;
+        else if(tolowercase(s[i])!=tolowercase(s[j]))
+            return false;
+        else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+    }
+};
